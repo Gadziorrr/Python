eec61a4dd97aece280c04047591f2254c1ca6797
Gadzior <73890050+Gadziorrr@users.noreply.github.com>
2023-03-18 19:14:03 +0100

    Zad 3 lista 1

---
+++ Zad3_List2.py
@@ -0,0 +1,21 @@
+class Rectangle:
+    def __init__(self, length, height):
+        self.length = length
+        self.height = height
+
+    def __str__(self):
+        return f"{type(self).__name__}\n" \
+               f"długość: {self.length}\n" \
+               f"wysokość: {self.height} \n" \
+               f"pole: {self.area}"
+
+    @property
+    def area(self):
+        return self.length * self.height
+
+    def __repr__(self):
+        return f"{type(self).__name__}"
+
+
+rect = Rectangle(5, 6)
+print(rect.__dict__)
